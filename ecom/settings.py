fdc68b2e69c14f5f2e898403c41ee64e4c5c87eb
bthinkxdev <bthinkx@gmail.com>
2026-07-17 18:05:53 +0530

    pgdb updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -19,29 +19,16 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [BASE_DIR / 'templates'], 'APP_DIRS': True, 'OPTIONS': {'context_processors': ['django.template.context_processors.request', 'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages', 'app.context_processors.site_contact_context', 'app.context_processors.cart_context', 'app.context_processors.wishlist_context', 'app.context_processors.admin_message_badge', 'app.context_processors.delivery_settings', 'app.context_processors.home_section_flags', 'app.context_processors.admin_product_settings', 'app.context_processors.storefront_brand', 'app.context_processors.search_typed_suggestions']}}]
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
-_postgres_db = config('POSTGRES_DB', default='').strip()
-if _postgres_db:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': _postgres_db,
-            'USER': config('POSTGRES_USER'),
-            'PASSWORD': config('POSTGRES_PASSWORD', default=''),
-            'HOST': config('POSTGRES_HOST', default='localhost'),
-            'PORT': config('POSTGRES_PORT', default='5432'),
-            'CONN_MAX_AGE': config('DB_CONN_MAX_AGE', default=60, cast=int),
-            'OPTIONS': {
-                'connect_timeout': config('DB_CONNECT_TIMEOUT', default=10, cast=int),
-            },
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'}, {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'}, {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'}, {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}]
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
