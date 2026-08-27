598f0a3f18583b5763c8c89b3b32880d445f7bba
devanjana401 <anjanasasiaduvad@gmail.com>
2026-08-27 20:47:30 +0530

    checkout updated

--- app/auth_views.py
+++ app/auth_views.py
@@ -276,11 +276,19 @@ class AddressUpdateView(LoginRequiredMixin, FormView):
         messages.success(self.request, 'Address updated successfully.')
         return super().form_valid(form)
 
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+        return super().get_success_url()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['active_page'] = 'account'
         context['form_title'] = 'Edit Address'
         context['address'] = self.address
+        if self.request.GET.get('next'):
+            context['next'] = self.request.GET.get('next')
         return context
 
 class AddressDeleteView(LoginRequiredMixin, View):
